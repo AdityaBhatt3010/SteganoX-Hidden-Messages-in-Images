42ef310e015e8cfaf8477266db6a57fd740246e5
Aditya Bhatt <96762636+AdityaBhatt3010@users.noreply.github.com>
2025-04-02 14:09:44 +0530

    Create SteganoX_Minimal.py

---
+++ SteganoX_Minimal.py
@@ -0,0 +1,19 @@
+from utilities import encode_image, decode_image, stylish_heading
+
+def main():
+    stylish_heading()
+    choice = input("Enter 'e' to encode or 'd' to decode: ").strip().lower()
+    
+    if choice == 'e':
+        image_path = input("Enter image path: ")
+        secret_message = input("Enter the message to hide: ")
+        output_image_path = input("Enter output image path: ")
+        encode_image(image_path, secret_message, output_image_path)
+    elif choice == 'd':
+        image_path = input("Enter image path to decode: ")
+        decode_image(image_path)
+    else:
+        print("Invalid choice! Please enter 'e' for encode or 'd' for decode.")
+
+if __name__ == "__main__":
+    main()
